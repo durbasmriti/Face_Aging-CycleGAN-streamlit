48d0a2403afa2e862a679c3b214a0db0f2b0ff0e
durbasmriti <152951506+durbasmriti@users.noreply.github.com>
2024-06-28 02:11:57 +0530

    Update model.py

--- model.py
+++ model.py
@@ -153,6 +153,18 @@ st.write('Upload an image to transform it to an aged version.')
 
 G_AB, G_BA, D_A, D_B = load_models()
 
+from PIL import ImageFilter
+
+def sharpen_image(image):
+    return image.filter(ImageFilter.SHARPEN)
+
+# Modify the part of the script where you convert the tensor back to a PIL image and save it
+def save_image_tensor(image_tensor, file_name):
+    image_tensor = image_tensor.squeeze().cpu().detach()
+    image = transforms.ToPILImage()(image_tensor)
+    image = sharpen_image(image)  # Apply sharpening filter
+    image.save(file_name)
+
 uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
 if uploaded_file is not None:
     image = Image.open(uploaded_file).convert('RGB')
