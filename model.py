f0615dc652f2b33d97a698040b0814ea7e2f051f
durbasmriti <152951506+durbasmriti@users.noreply.github.com>
2024-06-28 04:59:25 +0530

    Update model.py

--- model.py
+++ model.py
@@ -1,177 +1,250 @@
+# import streamlit as st
+# import torch
+# from torchvision import transforms
+# import torch.nn as nn
+# from torchvision.utils import save_image
+# from PIL import Image
+# import numpy as np
+# import io
+# import itertools
+
+# input_shape = (3,40,40)
+# c,img_height,img_width = input_shape
+
+# class ResidualBlock(nn.Module):
+#     def __init__(self, in_features):
+#         super(ResidualBlock, self).__init__()
+#         # Pad --> Conv2d (Same) --> IntanceNorm2d --> Relu --> Pad --> Conv2d (Same)  --> IntanceNorm2d
+#         self.block = nn.Sequential(
+#             nn.ReflectionPad2d(1),
+#             nn.Conv2d(in_features, in_features, 3),
+#             nn.InstanceNorm2d(in_features),
+#             nn.ReLU(inplace=True),
+#             nn.ReflectionPad2d(1),
+#             nn.Conv2d(in_features, in_features, 3),
+#             nn.InstanceNorm2d(in_features),
+#         )
+
+#     def forward(self, x):
+#         return x + self.block(x)
+
+# class GeneratorResNet(nn.Module):
+#     def __init__(self, input_shape, num_residual_blocks=9):
+#         '''
+#         input_shape : Tensor in (C,H,W) Format
+#         num_residual_blocks : Number of Residual blocks
+#         '''
+#         super(GeneratorResNet, self).__init__()
+
+#         channels = input_shape[0]
+
+#         # Initial convolution block
+#         out_features = 64
+#         model = [
+#             nn.ReflectionPad2d(channels),
+#             nn.Conv2d(channels, out_features, 7),
+#             nn.InstanceNorm2d(out_features),
+#             nn.ReLU(inplace=True),
+#         ]
+#         in_features = out_features
+
+#         # Downsampling
+#         for _ in range(2):
+#             out_features *= 2
+#             model += [
+#                 nn.Conv2d(in_features, out_features, 3, stride=2, padding=1),
+#                 nn.InstanceNorm2d(out_features),
+#                 nn.ReLU(inplace=True),
+#             ]
+#             in_features = out_features
+
+#         # Residual blocks
+#         for _ in range(num_residual_blocks):
+#             model += [ResidualBlock(out_features)]
+
+#         # Upsampling
+#         for _ in range(2):
+#             out_features //= 2
+#             model += [
+#                 nn.Upsample(scale_factor=2),
+#                 nn.Conv2d(in_features, out_features, 3, stride=1, padding=1),
+#                 nn.InstanceNorm2d(out_features),
+#                 nn.ReLU(inplace=True),
+#             ]
+#             in_features = out_features
+
+#         # Output layer
+#         model += [nn.ReflectionPad2d(channels), nn.Conv2d(out_features, channels, 7), nn.Tanh()]
+
+#         self.model = nn.Sequential(*model) #Build Sequential model by list unpacking
+
+#     def forward(self, x):
+#         return self.model(x)
+        
+# class Discriminator(nn.Module):
+#     def __init__(self, input_shape):
+#         super(Discriminator, self).__init__()
+
+#         channels, height, width = input_shape
+
+#         # Calculate output shape of image discriminator (PatchGAN)
+#         self.output_shape = (1, height // 2 ** 4, width // 2 ** 4)
+
+
+
+#         self.model = nn.Sequential(
+#             *self.discriminator_block(channels, 64, normalize=False),
+#             *self.discriminator_block(64, 128),
+#             *self.discriminator_block(128, 256),
+#             *self.discriminator_block(256, 512),
+#             nn.ZeroPad2d((1, 0, 1, 0)), # 
+#             nn.Conv2d(512, 1, 4, padding=1)
+#         )
+    
+#     def discriminator_block(self,in_filters, out_filters, normalize=True):
+#         """Returns downsampling layers of each discriminator block"""
+#         layers = [nn.Conv2d(in_filters, out_filters, 4, stride=2, padding=1)]
+#         if normalize:
+#             layers.append(nn.InstanceNorm2d(out_filters))
+#         layers.append(nn.LeakyReLU(0.2, inplace=True))
+#         return layers
+
+#     def forward(self, img):
+#         return self.model(img)
+        
+# def load_models():
+#     device = 'cuda' if torch.cuda.is_available() else 'cpu'
+#     G_AB = GeneratorResNet(input_shape, num_residual_blocks=3)
+#     G_BA = GeneratorResNet(input_shape, num_residual_blocks=3)
+#     D_A = Discriminator(input_shape)
+#     D_B = Discriminator(input_shape)
+
+#     G_AB.load_state_dict(torch.load("G_AB_99.pth", map_location=device), strict=False)
+#     G_BA.load_state_dict(torch.load("G_BA_99.pth", map_location=device), strict=False)
+#     D_A.load_state_dict(torch.load("D_A_99.pth", map_location=device), strict=False)
+#     D_B.load_state_dict(torch.load("D_B_99.pth", map_location=device), strict=False)
+
+#     G_AB.eval()
+#     G_BA.eval()
+#     D_A.eval()
+#     D_B.eval()
+
+#     return G_AB, G_BA, D_A, D_B
+    
+# G_AB, G_BA, D_A, D_B = load_models()
+
+# def transform_image(image):
+#     transform = transforms.Compose([
+#         transforms.ColorJitter(brightness=0.1, contrast=0.1, saturation=0.1, hue=0.1),
+#         transforms.ToTensor(),
+#         transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+        
+#     ])
+#     return transform(image).unsqueeze(0)
+
+# def generate_image(model, image_tensor):
+#     device = 'cuda' if torch.cuda.is_available() else 'cpu'
+#     image_tensor = image_tensor.to(device)
+#     with torch.no_grad():
+#         fake_image = model(image_tensor)
+#     return fake_image
+    
+# from PIL import ImageFilter
+
+# def sharpen_image(image):
+#     return image.filter(ImageFilter.SHARPEN)
+
+# def save_image_tensor(image_tensor, file_name):
+#     image_tensor = image_tensor.squeeze().cpu().detach()
+#     image = transforms.ToPILImage()(image_tensor)
+#     image = sharpen_image(image) 
+#     image.save(file_name)
+
+# st.title('CycleGAN Age Transformation')
+# st.write('Upload an image to transform it to an aged version.')
+
+# uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
+# if uploaded_file is not None:
+#     image = Image.open(uploaded_file).convert('RGB')
+#     st.image(image, caption='Uploaded Image', use_column_width=True)
+
+#     image_tensor = transform_image(image)
+#     # save_image_tensor(image_tensor, 'transformed_image.jpg')
+#     # st.image('transformed_image.jpg', caption='Transformed Image', use_column_width=True)
+
+#     fake_image_tensor = generate_image(G_AB, image_tensor)
+#     save_image_tensor(fake_image_tensor, 'generated_image.jpg')
+#     st.image('generated_image.jpg', caption='Aged Image', use_column_width=True)
+
 import streamlit as st
 import torch
 from torchvision import transforms
-import torch.nn as nn
-from torchvision.utils import save_image
 from PIL import Image
-import numpy as np
-import io
-import itertools
+import os
+import matplotlib.pyplot as plt
 
-input_shape = (3,40,40)
-c,img_height,img_width = input_shape
+# Define paths to the models
+# model_paths = {
+#     'G_AB': 'saved_models/your_dataset_name/G_AB_99.pth',
+#     'G_BA': 'saved_models/your_dataset_name/G_BA_99.pth',
+# }
 
-class ResidualBlock(nn.Module):
-    def __init__(self, in_features):
-        super(ResidualBlock, self).__init__()
-        # Pad --> Conv2d (Same) --> IntanceNorm2d --> Relu --> Pad --> Conv2d (Same)  --> IntanceNorm2d
-        self.block = nn.Sequential(
-            nn.ReflectionPad2d(1),
-            nn.Conv2d(in_features, in_features, 3),
-            nn.InstanceNorm2d(in_features),
-            nn.ReLU(inplace=True),
-            nn.ReflectionPad2d(1),
-            nn.Conv2d(in_features, in_features, 3),
-            nn.InstanceNorm2d(in_features),
-        )
+# Load the models
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-    def forward(self, x):
-        return x + self.block(x)
+# Assuming you have the GeneratorResNet class defined as in your previous code
+input_shape = (3, 40, 40)
+G_AB = GeneratorResNet(input_shape).to(device)
+G_BA = GeneratorResNet(input_shape).to(device)
 
-class GeneratorResNet(nn.Module):
-    def __init__(self, input_shape, num_residual_blocks=9):
-        '''
-        input_shape : Tensor in (C,H,W) Format
-        num_residual_blocks : Number of Residual blocks
-        '''
-        super(GeneratorResNet, self).__init__()
+G_AB.load_state_dict(torch.load(model_paths['G_AB_99.pth'], map_location=device))
+G_BA.load_state_dict(torch.load(model_paths['G_BA_99.pth'], map_location=device))
 
-        channels = input_shape[0]
+G_AB.eval()
+G_BA.eval()
 
-        # Initial convolution block
-        out_features = 64
-        model = [
-            nn.ReflectionPad2d(channels),
-            nn.Conv2d(channels, out_features, 7),
-            nn.InstanceNorm2d(out_features),
-            nn.ReLU(inplace=True),
-        ]
-        in_features = out_features
+# Define image transformations
+transform = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+])
 
-        # Downsampling
-        for _ in range(2):
-            out_features *= 2
-            model += [
-                nn.Conv2d(in_features, out_features, 3, stride=2, padding=1),
-                nn.InstanceNorm2d(out_features),
-                nn.ReLU(inplace=True),
-            ]
-            in_features = out_features
+# Define inverse transform for displaying images
+inverse_transform = transforms.Compose([
+    transforms.Normalize((-1, -1, -1), (2, 2, 2)),
+    transforms.ToPILImage()
+])
 
-        # Residual blocks
-        for _ in range(num_residual_blocks):
-            model += [ResidualBlock(out_features)]
+# Streamlit UI
+st.title("CycleGAN Image Translation")
 
-        # Upsampling
-        for _ in range(2):
-            out_features //= 2
-            model += [
-                nn.Upsample(scale_factor=2),
-                nn.Conv2d(in_features, out_features, 3, stride=1, padding=1),
-                nn.InstanceNorm2d(out_features),
-                nn.ReLU(inplace=True),
-            ]
-            in_features = out_features
-
-        # Output layer
-        model += [nn.ReflectionPad2d(channels), nn.Conv2d(out_features, channels, 7), nn.Tanh()]
-
-        self.model = nn.Sequential(*model) #Build Sequential model by list unpacking
-
-    def forward(self, x):
-        return self.model(x)
-        
-class Discriminator(nn.Module):
-    def __init__(self, input_shape):
-        super(Discriminator, self).__init__()
-
-        channels, height, width = input_shape
-
-        # Calculate output shape of image discriminator (PatchGAN)
-        self.output_shape = (1, height // 2 ** 4, width // 2 ** 4)
-
-
-
-        self.model = nn.Sequential(
-            *self.discriminator_block(channels, 64, normalize=False),
-            *self.discriminator_block(64, 128),
-            *self.discriminator_block(128, 256),
-            *self.discriminator_block(256, 512),
-            nn.ZeroPad2d((1, 0, 1, 0)), # 
-            nn.Conv2d(512, 1, 4, padding=1)
-        )
-    
-    def discriminator_block(self,in_filters, out_filters, normalize=True):
-        """Returns downsampling layers of each discriminator block"""
-        layers = [nn.Conv2d(in_filters, out_filters, 4, stride=2, padding=1)]
-        if normalize:
-            layers.append(nn.InstanceNorm2d(out_filters))
-        layers.append(nn.LeakyReLU(0.2, inplace=True))
-        return layers
-
-    def forward(self, img):
-        return self.model(img)
-        
-def load_models():
-    device = 'cuda' if torch.cuda.is_available() else 'cpu'
-    G_AB = GeneratorResNet(input_shape, num_residual_blocks=3)
-    G_BA = GeneratorResNet(input_shape, num_residual_blocks=3)
-    D_A = Discriminator(input_shape)
-    D_B = Discriminator(input_shape)
-
-    G_AB.load_state_dict(torch.load("G_AB_99.pth", map_location=device), strict=False)
-    G_BA.load_state_dict(torch.load("G_BA_99.pth", map_location=device), strict=False)
-    D_A.load_state_dict(torch.load("D_A_99.pth", map_location=device), strict=False)
-    D_B.load_state_dict(torch.load("D_B_99.pth", map_location=device), strict=False)
-
-    G_AB.eval()
-    G_BA.eval()
-    D_A.eval()
-    D_B.eval()
-
-    return G_AB, G_BA, D_A, D_B
-    
-G_AB, G_BA, D_A, D_B = load_models()
-
-def transform_image(image):
-    transform = transforms.Compose([
-        transforms.ColorJitter(brightness=0.1, contrast=0.1, saturation=0.1, hue=0.1),
-        transforms.ToTensor(),
-        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
-        
-    ])
-    return transform(image).unsqueeze(0)
-
-def generate_image(model, image_tensor):
-    device = 'cuda' if torch.cuda.is_available() else 'cpu'
-    image_tensor = image_tensor.to(device)
-    with torch.no_grad():
-        fake_image = model(image_tensor)
-    return fake_image
-    
-from PIL import ImageFilter
-
-def sharpen_image(image):
-    return image.filter(ImageFilter.SHARPEN)
-
-def save_image_tensor(image_tensor, file_name):
-    image_tensor = image_tensor.squeeze().cpu().detach()
-    image = transforms.ToPILImage()(image_tensor)
-    image = sharpen_image(image) 
-    image.save(file_name)
-
-st.title('CycleGAN Age Transformation')
-st.write('Upload an image to transform it to an aged version.')
-
+# File uploader
 uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
+
 if uploaded_file is not None:
-    image = Image.open(uploaded_file).convert('RGB')
-    st.image(image, caption='Uploaded Image', use_column_width=True)
+    # Read the uploaded image
+    image = Image.open(uploaded_file).convert("RGB")
 
-    image_tensor = transform_image(image)
-    # save_image_tensor(image_tensor, 'transformed_image.jpg')
-    # st.image('transformed_image.jpg', caption='Transformed Image', use_column_width=True)
+    # Transform the image
+    transformed_image = transform(image).unsqueeze(0).to(device)
 
-    fake_image_tensor = generate_image(G_AB, image_tensor)
-    save_image_tensor(fake_image_tensor, 'generated_image.jpg')
-    st.image('generated_image.jpg', caption='Aged Image', use_column_width=True)
+    # Generate images
+    fake_B = G_AB(transformed_image).squeeze(0).cpu().detach()
+    fake_A = G_BA(transformed_image).squeeze(0).cpu().detach()
+
+    # Inverse transform the images for display
+    fake_B_image = inverse_transform(fake_B)
+    fake_A_image = inverse_transform(fake_A)
+
+    # Display the original and transformed images
+    st.write("Original Image")
+    st.image(image, use_column_width=True)
+
+    st.write("Transformed Image (A -> B)")
+    st.image(fake_B_image, use_column_width=True)
+
+    st.write("Transformed Image (B -> A)")
+    st.image(fake_A_image, use_column_width=True)
+else:
+    st.write("Upload an image to see the translation results.")
+
