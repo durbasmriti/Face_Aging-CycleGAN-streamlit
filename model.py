97a164ae5d1649f824c4d4f89c59bbe1a35cd007
durbasmriti <152951506+durbasmriti@users.noreply.github.com>
2024-06-28 01:27:09 +0530

    Update model.py

--- model.py
+++ model.py
@@ -137,6 +137,7 @@ def load_models():
 def transform_image(image):
     transform = transforms.Compose([
         transforms.Resize(int(img_height * 1.12), Image.BICUBIC),
+        transforms.RandomHorizontalFlip(),
         transforms.ToTensor(),
         transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
     ])
